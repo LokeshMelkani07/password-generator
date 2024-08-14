652f7dafa3a127ffa0ac48b5bd93dc2327bfd7d6
LokeshMelkani07 <lokeshmelkani5102001@gmail.com>
2024-08-14 14:03:52 +0530

    Setting states and using useState

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
 
 // Yup for form validation
 import * as Yup from 'yup';
@@ -12,6 +12,18 @@ const PasswordSchema = Yup.object().shape({
 });
 
 export default function App() {
+  const [password, setPassword] = useState('');
+  const [isPassGenerated, setIsPassGenerated] = useState(false);
+  const [lowerCase, setLowerCase] = useState(false);
+  const [uppercase, setUpperCase] = useState(false);
+  const [numbers, setNumbers] = useState(false);
+  const [symbols, setSymbols] = useState(false);
+
+  const generatePasswordString = (passwordLength: number) => {};
+
+  const createPassword = (characters: string, passwordLength: number) => {};
+
+  const resetPassword = () => {};
   return (
     <View>
       <Text>App</Text>
